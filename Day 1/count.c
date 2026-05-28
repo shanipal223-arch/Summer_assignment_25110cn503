86a7388562116ff682cd9b6ada498378db2fbfa9
Shani pal <shanipal223@gmail.com>
2026-05-28 12:40:24 +0530

    Update count.c

--- Day 1/count.c
+++ Day 1/count.c
@@ -7,23 +7,23 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Keep track of the original number for printing later
+    
     int original_num = num;
 
-    // Handle 0 specifically
+    
     if (num == 0) {
         count = 1;
     }
 
-    // Convert negative numbers to positive
+    
     if (num < 0) {
         num = -num;
     }
 
-    // Simple loop to strip digits one by one
+    
     while (num > 0) {
-        num = num / 10; // Eliminates the last digit
-        count++;        // Adds 1 to the digit count
+        num = num / 10; 
+        count++;        
     }
 
     printf("The number %d has %d digits.\n", original_num, count);
