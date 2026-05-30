83b1daba5ba8ee830f753246d4f91f640a787eba
Shani pal <shanipal223@gmail.com>
2026-05-30 15:38:08 +0530

    Add files via upload

---
+++ day 3/GCD.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    
+    while (num1 != num2) {
+        if (num1 > num2) {
+            num1 = num1 - num2;
+        } else {
+            num2 = num2 - num1;
+        }
+    }
+
+    
+    printf("GCD is: %d\n", num1);
+
+    return 0;
+}
---
+++ day 3/rangeprime.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int lower, upper, i, j, isPrime;
+
+    
+    printf("Enter lower bound: ");
+    scanf("%d", &lower);
+    printf("Enter upper bound: ");
+    scanf("%d", &upper);
+
+    printf("Prime numbers: ");
+
+    
+    for (i = lower; i <= upper; i++) {
+        
+        if (i <= 1) {
+            continue;
+        }
+
+        isPrime = 1; 
+
+        
+        for (j = 2; j < i; j++) {
+            if (i % j == 0) {
+                isPrime = 0; 
+                break;      
+            }
+        }
+
+        // If it is still prime, print it
+        if (isPrime == 1) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
